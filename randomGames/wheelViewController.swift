ed2ed369ecb1d8bed295c3f3d8192b068b232a85
Howe <howewu1991@gmail.com>
2023-12-08 13:00:46 +0800

    dices code 50 %

---
+++ randomGames/wheelViewController.swift
@@ -0,0 +1,29 @@
+//
+//  wheelViewController.swift
+//  randomGames
+//
+//  Created by Howe on 2023/12/5.
+//
+
+import UIKit
+
+class wheelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
